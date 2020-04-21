b02a84199b9b62db38a8927c9a00898a7ed64f37
Ra <ramunasmon@gmail.com>
2020-04-21 12:01:39 +0300

    prisijungimas prie firebase

---
+++ app/src/main/java/lt/reactseals/akademija/filmu_app/MainActivity.java
@@ -0,0 +1,34 @@
+package lt.reactseals.akademija.filmu_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import lt.reactseals.akademija.filmu_app.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        button = (Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
