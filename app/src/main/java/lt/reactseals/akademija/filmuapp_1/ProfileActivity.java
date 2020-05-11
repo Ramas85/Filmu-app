46f2fa76d8475d030e04acbdd1a1f09669358ee7
Ra <ramunasmon@gmail.com>
2020-05-11 18:47:10 +0300

    ProfileActivity with new funcion (Navigation Drawer with Fragments) and logout funcion

--- app/src/main/java/lt/reactseals/akademija/filmuapp_1/ProfileActivity.java
+++ app/src/main/java/lt/reactseals/akademija/filmuapp_1/ProfileActivity.java
@@ -1,14 +1,21 @@
 package lt.reactseals.akademija.filmuapp_1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
 
@@ -17,17 +24,66 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+
+//Fragment dalis,reikalinga uzkrautu tam tikra fragmenta.
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new PopularFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_popular);
+        }
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new LatestFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_latest);
+        }
+
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_popular:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PopularFragment()).commit();
+                break;
+            case R.id.nav_latest:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new LatestFragment()).commit();
+                break;
+            case R.id.nav_willsoon:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SoonFragment()).commit();
+                break;
+
+            //TODO Parasyti,kodo fragmenta ka jis turetu daryti
+//            case R.id.nav_favorite:
+//                Toast.makeText(this, "favorite", Toast.LENGTH_SHORT).show();
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                //jeigu norim issilogint nesinaudojant Firebase
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent (ProfileActivity.this, MainActivity.class);
+                //Clear Stack(isvalo stack,task)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+               // startActivity(new Intent(ProfileActivity.this , MainActivityTwo.class));
+                startActivity(intent);
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     @Override
     public void onBackPressed() {
